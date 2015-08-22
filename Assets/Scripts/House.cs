6e642a44429f962b78aeaa50d25de46b2d02e20b
Edralo <anthony.baussard@gmail.com>
2015-08-22 13:18:52 +0200

    v1 house

---
+++ Assets/Scripts/House.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class House : MonoBehaviour {
+
+	public float switchDelay;
+	public bool state;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(switchState());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public IEnumerator switchState() {
+		while(true){
+			Debug.Log("begin");
+			yield return new WaitForSeconds(switchDelay);
+			state = !state;
+			Debug.Log("End");
+		}
+	}
+}
