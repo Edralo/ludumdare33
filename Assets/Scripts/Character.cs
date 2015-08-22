ec9ab52a7cd2670ca4253823464ab0030c39d3c9
cherif younis <cherif@macbook-air-de-cherif.home>
2015-08-22 12:21:55 +0200

    CharacterControls2D and Smooth follow Camera

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour 
+{
+	public Vector3 m_axis;
+	public float m_horizontalSpeed = 2f;
+	public float m_verticalSpeed = 2f;
+
+	public Rigidbody m_rb;
+	void Awake ()
+	{
+		m_axis = new Vector3();
+		m_rb = GetComponent<Rigidbody>();
+	}
+
+	void Update ()
+	{
+		// Get Input
+		m_axis.x = m_horizontalSpeed * Input.GetAxis("Horizontal");
+		m_axis.y = m_verticalSpeed * Input.GetAxis("Vertical");
+
+		// Move Witch
+
+		m_rb.MovePosition(transform.position + m_axis * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/SmoothFollow.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmoothFollow: MonoBehaviour 
+{
+	
+	public float dampTime = 0.15f;
+	private Vector3 velocity = Vector3.zero;
+	public Transform target;
+
+	void Start()
+	{
+		if( target == null ) 
+		{
+			target = GameObject.FindGameObjectWithTag ( "Heros" ).transform;
+		}
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if ( target )
+		{
+			Vector3 point = GetComponent<Camera>().WorldToViewportPoint( target.position );
+			Vector3 delta = target.position - GetComponent<Camera>().ViewportToWorldPoint( new Vector3( 0.5f, 0.5f, point.z ) );
+			Vector3 destination = transform.position + delta;
+			transform.position = Vector3.SmoothDamp( transform.position, destination, ref velocity, dampTime );
+		}
+		else
+		{
+			Debug.Log ( "Camera Doesn't found the gameobject tagged Heros to perform a smooth follow" );
+		}
+		
+	}
+}
