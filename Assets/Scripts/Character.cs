c6f38206da03437586f785240407f4cc0edf9a1a
cherif younis <cherif@macbook-air-de-cherif.home>
2015-08-22 17:41:13 +0200

    new controls of witch

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -2,10 +2,70 @@
 using System.Collections;
 
 public class Character : MonoBehaviour 
+{ 
+
+	public float m_speed; 
+	public int m_jumpHeight;
+	public bool m_isGrounded = false;
+	public int m_resources = 0;
+
+	
+	void FixedUpdate () 
+	{
+		Movement ();
+	}
+	
+	void Movement()
+	{
+		if (Input.GetKey (KeyCode.RightArrow)) 
+		{
+			transform.Translate (Vector2.right * m_speed * Time.deltaTime);
+			transform.eulerAngles = new Vector2(0,0); 
+		}
+		if (Input.GetKey (KeyCode.LeftArrow)) 
+		{
+			transform.Translate (Vector2.right * m_speed * Time.deltaTime);
+			transform.eulerAngles = new Vector2(0,180); 
+		}
+		
+	}
+	void Update()
+	{
+		if (Input.GetKeyDown (KeyCode.UpArrow) && m_isGrounded == true)
+		{
+			GetComponent<Rigidbody>().AddForce (Vector3.up * m_jumpHeight);
+		}
+	}
+
+	void OnCollisionEnter( Collision collision )
+	{
+		Debug.Log( collision.collider.name );
+		if ( collision.collider.CompareTag( "Ground" ) )
+		{
+			m_isGrounded = true;
+		}
+	}
+
+	void OnCollisionExit ( Collision collision ) 
+	{
+		Debug.Log( collision.collider.name );
+		if( collision.collider.CompareTag( "Ground" ) )
+		{
+			m_isGrounded = false;
+		}
+	}
+	
+}
+
+
+/*using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour 
 {
 	public Vector3 m_axis;
 	public float m_horizontalSpeed = 2f;
-	public float m_verticalSpeed = 2f;
+	public float m_verticalSpeed = 4f;
 	public int m_resources = 0;
 
 	public Rigidbody m_rb;
@@ -19,10 +79,18 @@ public class Character : MonoBehaviour
 	{
 		// Get Input
 		m_axis.x = m_horizontalSpeed * Input.GetAxis("Horizontal");
-		m_axis.y = m_verticalSpeed * Input.GetAxis("Vertical");
-
+		if(m_rb.velocity.y >= 0f )
+		{
+			m_axis.y = m_verticalSpeed * Input.GetAxis("Vertical");
+		}
 		// Move Witch
 
 		m_rb.MovePosition(transform.position + m_axis * Time.deltaTime);
 	}
+
+	void OnGUI()
+	{
+		GUILayout.Button(m_rb.velocity + "");
+	}
 }
+*/
