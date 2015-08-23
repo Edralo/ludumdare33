db14e03e3c745c2ffa4460c6d52217dc5bd5989d
Edralo <anthony.baussard@gmail.com>
2015-08-23 19:33:12 +0200

    clean spear

--- Assets/Scripts/Spear.cs
+++ Assets/Scripts/Spear.cs
@@ -2,13 +2,5 @@
 using System.Collections;
 
 public class Spear : Weapon {
-
-	// Update is called once per frame
-	void Update () {
-		if(GetComponent<Rigidbody>().velocity != Vector3.zero){
-			Quaternion rotation = new Quaternion();
-			rotation.SetLookRotation( GetComponent<Rigidbody>().velocity,Vector3.up );
-			transform.localRotation = rotation;
-		}
-	}
+	
 }
