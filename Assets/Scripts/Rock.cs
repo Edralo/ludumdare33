adb302cbb819d2a8292670ccab0d12e29f87d0ee
Edralo <anthony.baussard@gmail.com>
2015-08-25 02:29:52 +0200

    rgherh

--- Assets/Scripts/Rock.cs
+++ Assets/Scripts/Rock.cs
@@ -2,5 +2,19 @@
 using System.Collections;
 
 public class Rock : Weapon {
+	public float m_distanceToDestroy;
+	Transform m_witch;
 	
+	void Start()
+	{
+		m_witch = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	void Update()
+	{
+		if( Vector3.Distance ( transform.position, m_witch.position ) < m_distanceToDestroy )
+		{
+			Destroy ( gameObject );
+		}
+	}
 }
